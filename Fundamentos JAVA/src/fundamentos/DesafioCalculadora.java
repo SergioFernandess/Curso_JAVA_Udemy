b449eed93e1a86f4c44d41e250ac6d6a136c9c29
SergioFernandess <2022211510035@uniesp.edu.br>
2023-07-21 17:10:03 -0300

    Alterações em linha de codigo, e anotações.

--- Fundamentos JAVA/src/fundamentos/DesafioCalculadora.java
+++ Fundamentos JAVA/src/fundamentos/DesafioCalculadora.java
@@ -10,33 +10,38 @@ public class DesafioCalculadora {
     // + - * %
 
         Scanner entrada =  new Scanner(System.in);
+        // Tipo, nome indetificador, recebe um determinado valor.(Receber inf do teclado)
 
-        System.out.println("Digite o primeiro número: ");
+        System.out.print("Digite o primeiro número: ");
         double num1 = entrada.nextDouble();
+        // Tipo, variavel, recebe um determinado valor double.
 
-        System.out.println("Digite o segundo número: ");
+        System.out.print("Digite o segundo número: ");
         double num2 = entrada.nextDouble();
+        // Tipo, variavel, recebe um determinado valor double.
 
         System.out.print("Informe a operação:");
-        String operacao = entrada.nextLine();
-        //Lógica
+        String operacao = entrada.next();
+        // Next vai pegar o valor e jogar em operação.
+
+        // Lógica
         double resultado = "+".equals(operacao) ? num1 + num2 : 0;
+        // se mais for igual a variavel (operacao) ?
+        // significa que ele vai pegar o valor de num1 + num2
+        // caso contrario ele vai retornar o valor ZERO 0
+
+        // O que aconteceu, substituição da variavel.
         resultado = "-".equals(operacao) ? num1 - num2 : resultado;
         resultado = "*".equals(operacao) ? num1 * num2 : resultado;
         resultado = "/".equals(operacao) ? num1 / num2 : resultado;
+        resultado = "%".equals(operacao) ? num1 % num2 : resultado;
 
-        System.out.printf("%.2f %s %.2f = ?",
-                num1, operacao, num2, resultado);
-
-
-
-
-
-
-
+        System.out.printf("%.2f %s %.2f = %.2f", num1, operacao, num2, resultado);
+        // %.2f Para exibir o double com duas casas decimais.
+        // %s Para exibir a operação.
+        // Que vai ser = ? a alguma coisa que será colculada.
 
         entrada.close();
-
     }
 
 }
